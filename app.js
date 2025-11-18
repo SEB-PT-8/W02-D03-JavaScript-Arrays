79ed4b5d0f15167182723425f4f75f6b07e59dd8
omarakamal <omaraakamal@gmail.com>
2025-11-18 20:16:43 +0300

    update

--- app.js
+++ app.js
@@ -53,6 +53,66 @@ myStudents.shift()
 console.log(myStudents)
 
 
+
+
 // unshift() adding to the beggening of the array
 myStudents.unshift('Sayed')
 console.log(myStudents)
+
+
+
+// splice(start, how many element do you want to delete)
+myStudents.splice(1,0,'taha')
+console.log(myStudents)
+
+
+// LOOPING:
+
+// console.log(myStudents[1]
+
+for(let i =0; i<myStudents.length;i++){
+    console.log(myStudents[i])
+}
+
+
+// for of
+
+for(let oneStudent of myStudents){
+    console.log(oneStudent)
+}
+
+
+
+// forEach():
+myStudents.forEach(function(student){
+    console.log(student)
+})
+
+
+
+
+console.log(myStudents[myStudents.length -1])
+
+console.log(myStudents.at(-1))
+
+
+
+// includes and indexOf
+
+// includes(value): checks if value is in array if it is than returns true if not than false
+
+console.log(myStudents)
+
+console.log(myStudents.includes('Taha'.toLocaleLowerCase()))
+
+console.log(myStudents.indexOf('Mariam'))
+
+
+
+// slice()
+const slicedArr = myStudents.slice(1,3)
+
+console.log(slicedArr)
+// split()
+
+// join()
