00a3fd18000c92636010ca7c1e797ec5adcd794a
omarakamal <omaraakamal@gmail.com>
2025-11-18 18:40:19 +0300

    update

---
+++ app.js
@@ -0,0 +1,40 @@
+/* 
+Data types:
+
+1. Strings
+2. Number
+3. Boolean
+4. undefined
+5. null
+
+6. Arrays
+7. Objects
+
+*/
+
+// declare an array
+const myStudents = ['Mohammad','Sarah','Ahmed']
+
+// accessing a value in my array
+console.log(myStudents[1])
+
+
+// change a value in the array
+myStudents[1] = 'Ebrahim'
+
+console.log(myStudents)
+
+
+console.log(myStudents.length)
+
+
+const myName = 'Omar'
+
+console.log(myName.toUpperCase())
+
+
+// push(): adds an element to the end of the array
+
+myStudents.push('Husain')
+
+console.log(myStudents)
